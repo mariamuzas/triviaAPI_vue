43cee55635b02808aba8d86db1da64964a750975
Your Name <mmuzasantuna@gmail.com>
2020-10-24 15:25:05 +0100

    create component files, API fetched

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <main>
     <h1>Trivia Questions</h1>
-    
   </main>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      cards: []
+    }
+  }, 
+  
+  mounted() {
+    fetch('https://opentdb.com/api.php?amount=40&type=multiple')
+    .then(res => res.json())
+    .then(cards => this.cards = cards.results)
+  }
 }
 </script>
 
